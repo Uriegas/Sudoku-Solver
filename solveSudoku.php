e2aee5d612279f14bd6c18eeebd2531e1bcee85f
1930526@upv.edu.mx <1930526@upv.edu.mx>
2022-01-30 16:03:22 -0600

    Solve task

---
+++ solveSudoku.php
@@ -0,0 +1,7 @@
+<?php
+include 'backend.php';
+$table = json_decode($_POST['tablero']);
+$solution = Sudoku::solve($table);
+header('Content-type: application/json');
+echo(json_encode($solution));
+?>
